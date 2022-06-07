83516cfdd0107596562f8015da44e959fc1de842
Filip Pacnik <98559002+filip-pacnik@users.noreply.github.com>
2022-06-07 23:55:57 +0200

    addition of the 'library.properties' & 'kywrd.h'

---
+++ src/kywrd.h
@@ -0,0 +1,46 @@
+#ifndef KYWRD_H
+#define KYWRD_H
+
+
+
+
+#include <Arduino.h>
+
+
+
+
+#define PM pinMode
+
+
+#define I INPUT
+#define IP INPUT_PULLUP
+#define O OUTPUT
+
+
+
+
+#define DW digitalWrite
+
+
+#define L LOW
+#define H HIGH
+
+
+
+
+#define D delay
+#define DM delayMicroseconds
+
+
+
+
+#define SO sizeof
+
+
+#define Z 0
+#define ST 1
+
+
+
+
+#endif
